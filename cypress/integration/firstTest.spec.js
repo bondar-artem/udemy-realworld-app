f2c79f53c4fc4b0b49b1ecb5a02615638573214d
artembondar <artem.bondar@aa.com>
2020-01-20 14:03:34 -0600

    browser requests

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -6,8 +6,25 @@ describe('Test with backend', () => {
         cy.loginToApplication()
     })
 
-    it('should log in', () => {
-        cy.log('Yeeey we logged in!')
+    it('verify correct request and response', () => {
+        
+        cy.server()
+        cy.route('POST', '**/articles').as('postArticles')
+
+        cy.contains('New Article').click()
+        cy.get('[formcontrolname="title"]').type('This is a title')
+        cy.get('[formcontrolname="description"]').type('This is a descritption')
+        cy.get('[formcontrolname="body"]').type('This is a body of the Article')
+        cy.contains('Publish Article').click()
+
+        cy.wait('@postArticles')
+        cy.get('@postArticles').then( xhr => {
+            console.log(xhr)
+            expect(xhr.status).to.equal(200)
+            expect(xhr.request.body.article.body).to.equal('This is a body of the Article')
+            expect(xhr.response.body.article.description).to.equal('This is a descritption')
+        })
+
     })
 
 })
