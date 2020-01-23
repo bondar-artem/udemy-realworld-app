f8955e08bb73371d65dcf351976cb34e10b1e5ab
artembondar <artem.bondar@aa.com>
2020-01-22 19:53:21 -0600

    cy route

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -3,10 +3,12 @@
 describe('Test with backend', () => {
 
     beforeEach('login to the app', () => {
+        cy.server()
+        cy.route('GET', '**/tags', 'fixture:tags.json')
         cy.loginToApplication()
     })
 
-    it('verify correct request and response', () => {
+    it.skip('verify correct request and response', () => {
         
         cy.server()
         cy.route('POST', '**/articles').as('postArticles')
@@ -24,6 +26,36 @@ describe('Test with backend', () => {
             expect(xhr.request.body.article.body).to.equal('This is a body of the Article')
             expect(xhr.response.body.article.description).to.equal('This is a descritption')
         })
+
+    })
+
+    it('should gave tags with routing object', () => {
+        cy.get('.tag-list')
+        .should('contain', 'cypress')
+        .and('contain', 'automation')
+        .and('contain', 'testing')
+    })
+
+    it('verify global feed likes count', () => {
+        cy.route('GET', '**/articles/feed*', '{"articles":[],"articlesCount":0}')
+        cy.route('GET', '**/articles*', 'fixture:articles.json')
+
+        cy.contains('Global Feed').click()
+        cy.get('app-article-list button').then( listOfbuttons => {
+            expect(listOfbuttons[0]).to.contain('1')
+            expect(listOfbuttons[1]).to.contain('5')
+
+        })
+
+        cy.fixture('articles').then( file => {
+            const articleLink = file.articles[1].slug
+            cy.route('POST', '**/articles/'+articleLink+'/favorite', file)
+        })
+
+        cy.get('app-article-list button')
+        .eq(1)
+        .click()
+        .should('contain', '6')
 
     })
 
