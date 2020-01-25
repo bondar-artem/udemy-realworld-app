77cf77e2b802259726b3988c9e1a31da5098aca5
artembondar <artem.bondar@aa.com>
2020-01-25 12:26:09 -0600

    cy request

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -59,4 +59,52 @@ describe('Test with backend', () => {
 
     })
 
+    it.only('delete a new article in a global feed', () => {
+
+        const userCredentials = {
+            "user": {
+                "email": "artem.bondar16@gmail.com",
+                "password": "CypressTest1"
+            }
+        }
+
+        const bodyRequest = {
+            "article": {
+                "tagList": [],
+                "title": "Request from API",
+                "description": "API testing is easy",
+                "body": "Angular is cool"
+            }
+        }
+
+        cy.request('POST', 'https://conduit.productionready.io/api/users/login', userCredentials)
+        .its('body').then(body => {
+            const token = body.user.token
+
+            cy.request({
+                url: 'https://conduit.productionready.io/api/articles/',
+                headers: { 'Authorization': 'Token '+token},
+                method: 'POST',
+                body: bodyRequest
+            }).then( response => {
+                expect(response.status).to.equal(200)
+            })
+
+            cy.contains('Global Feed').click()
+            cy.get('.article-preview').first().click()
+            cy.get('.article-actions').contains('Delete Article').click()
+
+            cy.request({
+                url: 'https://conduit.productionready.io/api/articles?limit=10&offset=0',
+                headers: { 'Authorization': 'Token '+token},
+                method: 'GET'
+            }).its('body').then( body => {
+                expect(body.articles[0].title).not.to.equal('Request from API')
+            })
+
+        })
+
+
+    })
+
 })
