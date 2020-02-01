c5c16028b4f44794eb159474a62dc7e7e6c7889f
artembondar <artem.bondar@aa.com>
2020-02-01 12:31:16 -0600

    cypress retry

--- cypress/integration/secondTest.spec.js
+++ cypress/integration/secondTest.spec.js
@@ -11,7 +11,7 @@ describe('Test log out', () => {
     it('verify use can log out successfully', () => {
         cy.contains('Settings').click()
         cy.contains('Or click here to logout').click()
-        cy.get('.navbar-nav').should('contain', 'Sign up')
+        cy.get('.navbar-nav').should('contain', 'Sign up1')
     })
 
 })
