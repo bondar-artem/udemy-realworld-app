fc49b8c9742dc0cd8c317cd7bbec50aa6e1380a8
Artem Bondar <artem.bondar@gmail.com>
2022-10-29 17:02:31 -0500

    deleted plugins

--- cypress.config.ts
+++ cypress.config.ts
@@ -17,11 +17,10 @@ export default defineConfig({
     runMode: 2,
     openMode: 0,
   },
+  
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      // return require('./cypress/plugins/index.js')(on, config)
+
     },
     baseUrl: 'http://localhost:4200/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
