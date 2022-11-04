5753ff1dece5547975af52148ceefe73c747c785
Artem Bondar <artem.bondar@gmail.com>
2022-11-04 16:39:23 -0500

    env vars

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,10 +4,27 @@ export default defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   video: false,
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+  },
+  env: {
+    username: 'cytest@test.com', 
+    password: 'Welcome123',
+    apiUrl: 'https://api.realworld.io'
+  },
   
   e2e: {
     setupNodeEvents(on, config) {
+      const username = process.env.DB_USERNAME
+      const password = process.env.PASSWORD
 
+      if(!password){
+        throw new Error(`missing PASSWORD environment variable`)
+      }
+
+      config.env = {username, password}
+      return config
     },
     baseUrl: 'http://localhost:4200/',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -28,12 +28,12 @@ Cypress.Commands.add('loginToApplication', () => {
     
     const userCredentials = {
         "user": {
-            "email": "cytest@test.com",
-            "password": "Welcome123"
+            "email": Cypress.env("username"),
+            "password": Cypress.env("password")
         }
     }
 
-    cy.request('POST', 'https://api.realworld.io/api/users/login', userCredentials)
+    cy.request('POST', Cypress.env('apiUrl')+'/api/users/login', userCredentials)
         .its('body').then( body => {
             const token = body.user.token
             cy.wrap(token).as('token')
